ef5a4a756236665a7aa8567f63134b6d6640839c
franciscocruz29 <franciscohuertas25@gmail.com>
2023-01-31 11:32:03 -0500

    React-JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 
+function getTitle(title) {
+  return title;
+}
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div>
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
